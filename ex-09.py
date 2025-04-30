4f2b3fac29eb370c6b5318ece7225b54ead8c23a
nicolascrf7 <nicolassoares125@gmail.com>
2025-04-29 21:32:23 -0300

    Create ex-09.py

---
+++ ex-09.py
@@ -0,0 +1,6 @@
+numero1= input ("digite numero1;")
+numero2= input ("digite numero2;")
+numero1= int(numero1)
+numero2= int(numero2)
+soma= numero1 + numero2
+print ("digite a soma dos dois numeros e;" ,soma)
