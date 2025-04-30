31eaba32857e78d596a2cb29403188bc277063d9
nicolascrf7 <nicolassoares125@gmail.com>
2025-04-29 21:51:19 -0300

    Create ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,8 @@
+nota1= float(input("digite a nota1:"))
+nota2= float(input("digite a nota2:"))
+nota3= float(input("digite a nota3:"))
+nota4= float(input("digite a nota4:"))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f"a média final é: {media:.2f}")
